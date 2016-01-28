289996f52d613a1268635eb94f2bdb7aa2d75685
jeinglis <jeinglis@ucalgary.ca>
2016-01-28 10:02:50 -0700

    Created-RangeTest-Case
    created the first test case but it doesn’t recognize the test case

---
+++ src/org/jfree/data/test/RangeTest.java
@@ -0,0 +1,29 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
+import org.jfree.data.Range;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RangeTest extends TestCase {
+	private Range exampleRange;
+
+	@Before
+	public void setUp() throws Exception {
+		exampleRange = new Range(-1,1);
+	}
+
+	@Test
+	public void CentralValueShouldBeZero() {
+		assertEquals("The central value of -1 and 1 should be 0",0,exampleRange.getCentralValue(), .000000001d);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+}
