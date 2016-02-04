13d3bdfced89c9dfe2c78bb9bfaead31d4d9fa3f
Right2Drive <bjpfannm@ucalgary.ca>
2016-02-04 10:46:08 -0700

    Finished every second test up to 10, working on 12 now.

---
+++ src/org/jfree/data/test/cases/RangeGetLengthTest.java
@@ -0,0 +1,22 @@
+package org.jfree.data.test.cases;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.Range;
+import org.junit.Test;
+
+public class RangeGetLengthTest {
+	private Range range;
+	
+	@Test
+	public void getLength_1() {
+		range = new Range(5, 5);
+		assertEquals("The length of (5, 5) should be 0", 0, range.getLength(), 0);
+	}
+	
+	@Test
+	public void getLength_2() {
+		range = new Range(0, 10);
+		assertEquals("The length of (0, 10) should be 10", 10, range.getLength(), 0);
+	}
+}
---
+++ src/org/jfree/data/test/cases/RangeIntersectsTest.java
@@ -0,0 +1,30 @@
+package org.jfree.data.test.cases;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.Range;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RangeIntersectsTest {
+	private Range range;
+	private double high;
+	private double low;
+	
+	@Before
+	public void setUp() throws Exception {
+		range = new Range(0, 10);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
